4f53cdd82b275bd06ca0dab9745b12e764eb0e95
bhagyarajuvarigeti1 <bhagyarajuvarigeti@gmail.com>
2022-06-24 20:55:59 +0530

    UC2 branch

--- EmployeeWageComputationProblem.java
+++ EmployeeWageComputationProblem.java
@@ -1,17 +1,19 @@
 public class EmployeeWageComputationProblem {
     public static void main(String[] args) {
-        int fullDayHour = 8, pratTimeHour = 8, wagePerHour = 20;
+        int fullDayHour=8,pratTimeHour=8,wagePerHour=20;
 
-        long totalWage = 0;
-        int days = 0, hours = 0;
-        while (days < 20 && hours < 100) {
-            double p = Math.random();
-            if (p < 0.3) {
-                totalWage += fullDayHour * wagePerHour;
-                hours += fullDayHour;
-            } else if (p > 0.3 && p < 0.6) {
-                totalWage += pratTimeHour * wagePerHour;
-                hours += fullDayHour;
+        long totalWage=0;
+        int days=0,hours=0;
+        while (days<20 && hours<100)
+        {
+            double p=Math.random();
+            if(p<0.3){
+                totalWage+=fullDayHour*wagePerHour;
+                hours+=fullDayHour;
+            }
+            else if (p>0.3 && p<0.6){
+                totalWage+=pratTimeHour*wagePerHour;
+                hours+=fullDayHour;
             }
             days++;
             System.out.println(totalWage);
